c586a22372ac999e81885e40981a31e355408403
pooja naicker <poojanaicker.y@gmail.com>
2025-08-21 12:13:45 +0530

    task.js

--- task.js
+++ task.js
@@ -1,8 +1,5 @@
 console.log("hello welcome to git class")
-<<<<<<< HEAD
 console.log("welcome to third class");
 
-=======
 console.log("welcome to second day")
 console.log("welcome to second day")
->>>>>>> b32ef81c4e6d111883d447f2ca0914cefb0f7101
